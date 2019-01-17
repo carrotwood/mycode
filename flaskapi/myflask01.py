fe1c0e4c114fd0be06d338e53697d767fe31312f
carrotwood <commstamiami@hotmail.com>
2019-01-17 15:14:47 -0500

    lab

--- flaskapi/myflask01.py
+++ flaskapi/myflask01.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+# An object of Flask class is our WSGI application
 from flask import Flask
 
+# Flask constructor takes the name of current module (__name__) as argument
 app = Flask(__name__)
 
+# route() function of the Flask class is a decorator, tells the application which URL should call the associated function
 @app.route('/')
 def hello_world():
    return 'Hello World'
 
 if __name__ == '__main__':
-   app.run(port=5006) 
+   app.run(port=5006) # runs the application on the local development server
+   # app.run(port=5006, debug=True) # DEBUG MODE - comment out line above before turning on
